5fcd18401d1cb15f011436f9781b86afdd47d1e3
hichem2231 <yatimhichem@gmail.com>
2025-12-22 01:09:35 +0100

    Add basic conditional examples using if/else

---
+++ conditions.js
@@ -0,0 +1,24 @@
+/*
+ checking conditions using if/else
+*/
+// Example 1 :
+ let score = 85;
+
+ if(score >= 90) {
+    console.log("Excellent! You got an A");
+ } else if(score >= 80) {
+    console.log("Very Good! You got an B");
+ } else if(score >= 50) {
+    console.log("Passed!"); 
+ } else {
+    console.log("Failed. Keep trying!");
+ }
+
+// Example 2 :
+let number = 10;
+
+if(number % 2 === 0) {
+    console.log(number +" is Even");
+} else {
+    console.log(number +" is Odd");
+}
