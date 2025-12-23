713d611ec62d59da54f2e325d1832241223da346
hichem2231 <yatimhichem@gmail.com>
2025-12-23 21:15:42 +0100

    Add basic loop examples

---
+++ loops.js
@@ -0,0 +1,24 @@
+// Example 1: Print iterations from 1 to 5 using for loop
+
+for (let i = 1; i <= 5; i++) {
+  console.log("Iteration number : "+ i);
+}
+
+// Example 2: Calculate the sum of numbers from 1 to 10
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+
+console.log("Sum : "+ sum);
+
+// Example 3: Print iterations from 1 to 5 using while loop
+
+let count = 1;
+
+while (count <= 5) {
+  console.log("Iteration number : "+ count);
+  count++;
+}
